0a6b0d4bad0bf4627c5f4a5855d3dda3a95af467
Prabhjeet Singh <148787330+prabhjeetsinghk@users.noreply.github.com>
2025-10-28 20:21:04 -0400

    Update JwtAuthenticationFilter.java

--- src/main/java/com/bank/config/JwtAuthenticationFilter.java
+++ src/main/java/com/bank/config/JwtAuthenticationFilter.java
@@ -29,6 +29,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+    String path = request.getServletPath();
+    return path.startsWith("/api/auth");
+    }
+
+
+    @Override
     protected void doFilterInternal(HttpServletRequest request,
             HttpServletResponse response,
             FilterChain filterChain) throws ServletException, IOException {
